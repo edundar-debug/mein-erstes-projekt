5b279d9be5c472f15b320d9b9f49dcc624b8401a
Emre Dundar <emredundar@Mac.fritz.box>
2026-09-10 19:03:13 +0200

    Berechnung der Gesamteinnahmen mit for-Schleife umgesetzt

--- main.py
+++ main.py
@@ -3,21 +3,31 @@
 km = 12
 
 if km <= 3:  
-    cost = 5.0  #Kurzstrecke
+    cost = 5.0  #short-haul
 elif km <= 10:  
-    cost = 5.0 * (km - 3) * 2.0  #Normaltarif
+    cost = 5.0 * (km - 3) * 2.0  #standard rate
 else:   
-    cost = 19.0 * (km - 10) *1.5  #Langstrecke (Günstigerer KM-Preis)
+    cost = 19.0 * (km - 10) *1.5  #Long-distance (lower price per km)
 
-print(f"Fahrpreis für {km} km: {cost} Euro")
+print(f"Fare for {km} km: {cost} euros")
 
 
-# Eine Liste mit den Fahrpreisen der letzten Kunden
+# A list of the fares paid by the last customers
 income = [8.5, 12.0, 5.0, 22.4]
 
-# Zugriff auf ein Element (Zählung beginnt bei 0!)
-print(f"Erste fahrt: {income}")
+# Access an element (indexing starts at 0!)
+print(f"First drive: {income}")
 
-# Ein neues Element am Ende hinzufügen 
+# Add a new element at the end
 income.append(15.3)
-print(f"Alle Einnahmen: {income}")
+print(f"All income: {income}")
+
+
+trips = [8.5, 12.0, 5.00, 22.40]
+total_revenue = 0
+
+# Go through each trip individually
+for trip in trips:
+     total_revenue = total_revenue + trip
+
+print(f"Daily revenue: {total_revenue} euros")
