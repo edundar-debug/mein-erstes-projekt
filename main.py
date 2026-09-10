9a6f21e398f259d195cb60e9a137056f84697e48
Emre Dundar <emredundar@Mac.fritz.box>
2026-09-10 18:12:30 +0200

    Erstellung von Liste mit Erweiterung durch append

--- main.py
+++ main.py
@@ -10,3 +10,14 @@ else:
     cost = 19.0 * (km - 10) *1.5  #Langstrecke (Günstigerer KM-Preis)
 
 print(f"Fahrpreis für {km} km: {cost} Euro")
+
+
+# Eine Liste mit den Fahrpreisen der letzten Kunden
+income = [8.5, 12.0, 5.0, 22.4]
+
+# Zugriff auf ein Element (Zählung beginnt bei 0!)
+print(f"Erste fahrt: {income}")
+
+# Ein neues Element am Ende hinzufügen 
+income.append(15.3)
+print(f"Alle Einnahmen: {income}")
